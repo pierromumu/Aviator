dd5c7f7989c811ed6227d30f5664e07aca02896b
pierre <pierromumu@gmail.com>
2016-11-02 15:35:43 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String sampleContent = "";
+        Path sampleFile = Paths.get("./files/Google.html");
+        try {
+            List<String> sampleLines = Files.readAllLines(sampleFile, StandardCharsets.UTF_8);
+            Iterator<String> sampleIterator = sampleLines.iterator();
+            while (sampleIterator.hasNext()) {
+                sampleContent += sampleIterator.next();
+            }
+
+            sampleContent = sampleLines.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Document sampleDocument = Jsoup.parse(sampleContent);
+
+        System.out.println(sampleDocument);
+    }
+}
