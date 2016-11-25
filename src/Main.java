6493661cf1586d9fde8036127744ac3380452eed
NeoKyno <kyno@live.fr>
2016-11-25 14:48:40 +0100

    Index now WAY MORE faster, completion for whole files in about 3 mins

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xml.internal.security.algorithms.implementations.IntegrityHmac;
 import com.sun.org.apache.xpath.internal.SourceTree;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.sql.*;
@@ -86,7 +88,7 @@ public class Main {
         }
     }
 
-    public static void insertWord(Connection c, String word, int docId){
+    public static void insertWord(Connection c, String word, int docId, int nb_occ){
 
         try {
             Statement stmt = c.createStatement();
@@ -98,11 +100,11 @@ public class Main {
             result.next();
 
             if (!(result.getBoolean("exists"))){
-                sql = "INSERT INTO vocabulary(WORD) VALUES ('"+word+"');";
+                sql = "INSERT INTO vocabulary(WORD, FREQ) VALUES ('"+word+"', '"+nb_occ+"');";
                 stmt.executeUpdate(sql);
             }
 
-            sql = "UPDATE vocabulary SET FREQ = FREQ + 1 WHERE WORD='"+word+"';";
+            sql = "UPDATE vocabulary SET FREQ = FREQ + "+nb_occ+" WHERE WORD='"+word+"';";
             stmt.executeUpdate(sql);
 
             // mise à jour de l'index inversé
@@ -118,15 +120,67 @@ public class Main {
             result.next();
 
             if (!(result.getBoolean("exists"))){
-                sql = "INSERT INTO invertedindex (WORD_ID, DOC_ID, OCC) VALUES ('"+word_id+"', '"+docId+"', '1');";
+                sql = "INSERT INTO invertedindex (WORD_ID, DOC_ID, OCC) VALUES ('"+word_id+"', '"+docId+"', '"+nb_occ+"');";
                 stmt.executeUpdate(sql);
             } else {
-                sql = "UPDATE invertedindex SET OCC = OCC +1 WHERE WORD_ID='"+word_id+"' AND DOC_ID='"+docId+"';";
+                sql = "UPDATE invertedindex SET OCC = OCC +"+nb_occ+" WHERE WORD_ID='"+word_id+"' AND DOC_ID='"+docId+"';";
                 stmt.executeUpdate(sql);
             }
 
             stmt.close();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void insertVoc(Connection c, HashMap<String, Integer[]> vocabulary) {
+
+        String sql = "INSERT INTO vocabulary(ID, WORD, FREQ) VALUES ";
+
+        for (String key : vocabulary.keySet()){
+            sql = sql+("("+vocabulary.get(key)[0]+", '"+key+"', "+vocabulary.get(key)[1]+"), ");
+        }
+
+        sql = sql.substring(0, sql.lastIndexOf(","));
+        sql = sql+";";
+        Statement stmt;
+        try {
+            stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void insertIndex(Connection c, HashMap<Integer, HashMap<Integer, Integer>> word_list){
+
+        String doc_sql = "INSERT INTO documents (ID, TITLE) VALUES ";
+        String words_sql = "INSERT INTO invertedindex (WORD_ID, DOC_ID, OCC) VALUES ";
+
+        for (int key : word_list.keySet()){
+            doc_sql = doc_sql+"("+key+", 'D"+key+"'), ";
+            for (int word : word_list.get(key).keySet()) {
+                words_sql = words_sql + "(" + word + ", " + key + ", " + word_list.get(key).get(word) + ") ,";
+            }
+        }
+
+        doc_sql = doc_sql.substring(0, doc_sql.lastIndexOf(","));
+        doc_sql = doc_sql+";";
+        words_sql = words_sql.substring(0, words_sql.lastIndexOf(","));
+        words_sql = words_sql+";";
+        Statement stmt;
+        try {
+            stmt = c.createStatement();
+
+            stmt.executeUpdate(doc_sql);
+            stmt.executeUpdate(words_sql);
+
+            stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -148,6 +202,19 @@ public class Main {
             // création des tables dans la base de données
             createTables(c);
 
+            HashMap<String, Integer[]> vocabulary = new HashMap<>();
+            //First element: Id, second element: value
+            int voc_id = 0;
+
+
+            //HashMap<Integer, Integer> word_in_doc = new HashMap<Integer, Integer>();
+
+            HashMap<Integer, HashMap<Integer, Integer>> doc_voc = new HashMap<>();
+            //First element: ID, second: freq
+            //HashMap<String, Integer> word_list = new HashMap<String, Integer>();
+
+
+            System.out.println("Starting Data gathering");
 
             for(int i=1;i<=138;i++) {
                 String documentName="D"+i;
@@ -172,17 +239,55 @@ public class Main {
 
                 //System.out.println("Texte original : " + currentDocument.text());
 
-                insertDocument(c, documentName, i);
+                //insertDocument(c, documentName, i);
+
+
+                //Cette hashmap contient les valeurs avant l'intégration dans la bdd, objectif: économiser les e/s de la bdd
+                HashMap<Integer, Integer> current_doc = new HashMap<>();
+
                 for (String word : words) {
+                    word = word.toLowerCase();
                     //System.out.println(word);
+                    int word_id;
+                    int word_occ;
                     word = stemTerm(word.replaceAll("[\\W]",""));
-                    insertWord(c, word, i);
+                    if (vocabulary.containsKey(word)){
+                        word_id = vocabulary.get(word)[0];
+                        word_occ = vocabulary.get(word)[1] +1;
+                        Integer[] current_word = {word_id, word_occ};
+                        vocabulary.put(word, current_word);
+                    } else {
+                        Integer[] current_word = {voc_id, 1};
+                        word_id = voc_id;
+                        voc_id++;
+                        vocabulary.put(word, current_word);
+                    }
+
+                    if (current_doc.containsKey(word_id)){
+                        current_doc.put(word_id, current_doc.get(word_id) + 1);
+                    } else {
+                        current_doc.put(word_id, 1);
+                    }
+
+                    doc_voc.put(i, current_doc);
+
                     //System.out.println(word);
                 }
 
 
+
+
                 System.out.println("Document "+documentName+".html successfully processed.");
             }
+
+            System.out.println("All data gathered, commencing index.");
+
+            insertVoc(c, vocabulary);
+            System.out.println("Vocabulary saved");
+
+            insertIndex(c, doc_voc);
+
+            System.out.println("Index Finished");
 
             c.close();
 
